0f89d569d18b49419db8a189dbfeefac961e4d52
Chen Tong <chentong@xover.com.my>
2024-01-07 13:44:33 +0800

    Update

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import {StyleSheet} from 'react-native';
 import {StatusBar} from "expo-status-bar";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <>
-            <StatusBar style={"light"}></StatusBar>
-            <CategoriesScreen/>
+            <StatusBar style={"dark"}></StatusBar>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="MealsCategories" component={CategoriesScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>
         </>
     );
 }
