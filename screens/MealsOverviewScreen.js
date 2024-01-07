d7b71c2c4acbb3d79584d6b6859d5a4d5860b72f
Chen Tong <chentong@xover.com.my>
2024-01-07 15:05:55 +0800

    Update

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,14 +1,25 @@
+import {useLayoutEffect} from "react";
 import {FlatList, StyleSheet, View} from "react-native";
 
 import MealItem from "../components/MealItem";
-import {MEALS} from '../data/dummy-data';
+import {CATEGORIES, MEALS} from '../data/dummy-data';
 
-function MealsOverviewScreen({route}) {
+function MealsOverviewScreen({route, navigation}) {
     const catId = route.params.categoryId;
 
     const displayedMeals = MEALS.filter((mealItem) => {
         return mealItem.categoryIds.indexOf(catId) >= 0;
     });
+
+    useLayoutEffect(() => {
+        const categoryTitle = CATEGORIES.find((category) => {
+            return category.id === catId;
+        }).title;
+
+        navigation.setOptions({
+            title: categoryTitle,
+        });
+    }, [catId, navigation]);
 
     function renderMealItem(itemData) {
         const item = itemData.item;
